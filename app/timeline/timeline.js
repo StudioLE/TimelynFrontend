d0140da799eecac5741ae94209845007d33e3f37
Laurence Elsdon <git@elsdon.me>
2015-03-08 02:14:46 +0000

    Renamed timeline controllers and timeline-preview.html

--- app/timeline/timeline.js
+++ app/timeline/timeline.js
@@ -4,20 +4,20 @@ angular.module('myApp.timeline', ['ngRoute', 'ngResource'])
 
 .config(['$routeProvider', function($routeProvider) {
 	$routeProvider.when('/timeline/edit/:timelineId/event/:eventId', {
-		templateUrl: 'timeline/timeline-preview.html',
-		controller: 'TimelinePreviewCtrl'
+		templateUrl: 'timeline/timeline.html',
+		controller: 'TimelineCtrl'
 	});
 	$routeProvider.when('/timeline/:action/:timelineId', {
-		templateUrl: 'timeline/timeline-preview.html',
-		controller: 'TimelinePreviewCtrl'
+		templateUrl: 'timeline/timeline.html',
+		controller: 'TimelineCtrl'
 	});
 	$routeProvider.when('/timeline/:timelineId', {
-		templateUrl: 'timeline/timeline-preview.html',
-		controller: 'TimelinePreviewCtrl'
+		templateUrl: 'timeline/timeline.html',
+		controller: 'TimelineCtrl'
 	});
 	$routeProvider.when('/timeline', {
 		templateUrl: 'timeline/timeline-list.html',
-		controller: 'TimelineCtrl'
+		controller: 'TimelineListCtrl'
 	});
 }])
 
@@ -27,7 +27,7 @@ angular.module('myApp.timeline', ['ngRoute', 'ngResource'])
 	});
 })
 
-.controller('TimelineCtrl', function($scope, Timeline, $location, $log) {
+.controller('TimelineListCtrl', function($scope, Timeline, $location, $log) {
 
 	// Get all Timelines for list
 	$scope.timelines = Timeline.query()
@@ -57,7 +57,7 @@ angular.module('myApp.timeline', ['ngRoute', 'ngResource'])
 
 })
 
-.controller('TimelinePreviewCtrl', ['$scope', '$routeParams', 'Timeline', 'Event', '$location', function ($scope, $routeParams, Timeline, Event, $location) {
+.controller('TimelineCtrl', ['$scope', '$routeParams', 'Timeline', 'Event', '$location', function ($scope, $routeParams, Timeline, Event, $location) {
 
 	// Get the timeline by routeParam
 	$scope.timeline = Timeline.get({id: $routeParams.timelineId});
@@ -213,57 +213,3 @@ angular.module('myApp.timeline', ['ngRoute', 'ngResource'])
 	};
 
 }])
-
-.controller('TimelineSaveCtrl', ['$scope', '$routeParams', 'Timeline', 'Event', '$location', function ($scope, $routeParams, Timeline, Event, $location) {
-		$scope.partial = 'events-form'
-
-	// ng-click="saveTimeline()"
-	$scope.saveTimeline = function() {
-
-		// REST success callback
-		var success = function(timeline, responseHeaders){
-			
-			// If new timeline then create the first event on it
-			if($routeParams.timelineId === 'create') {
-				Event.save({
-					"timeline": timeline.id,
-					"startDate": timeline.createdAt,
-					"endDate": "",
-					"headline": "I created my first Timeline using SailsTimeline",
-					"text": "<p>You can put some text here. Why, isn't that pretty...</p>",
-					"tag": "",
-					"classname": "",
-					"asset": {
-						"media": "",
-						"thumbnail": "http://lorempixel.com/32/32/",
-						"credit": "Credit Name Goes Here",
-						"caption": "Caption text goes here"
-					}
-				})
-			}
-
-			// Redirect on success
-			$location.path('/timeline/' + timeline.id);
-		}
-
-		// REST failure callback
-		var failure = function(httpResponse){
-			if($scope.errors === undefined) {
-				$scope.errors = []
-			}
-			// Error
-			$scope.errors.push(httpResponse);
-		}
-
-		// If action is edit
-		if($routeParams.timelineId !== 'create') {
-			Timeline.edit($scope.timeline, success, failure);
-		}
-		// Else action is create
-		else {
-			Timeline.save($scope.timeline, success, failure);
-		}
-		
-	};
-
-}]);
