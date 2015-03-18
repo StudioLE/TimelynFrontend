db53ef01845873c63ea310e4678b8a9ae250d72e
Laurence Elsdon <git@elsdon.me>
2015-03-18 02:12:13 +0000

    Dashboard fetch user

--- app/dashboard/dashboard.js
+++ app/dashboard/dashboard.js
@@ -2,6 +2,12 @@
 
 angular.module('myApp.dashboard', ['ngRoute'])
 
+
+/*****************************************************************
+*
+* Route provider
+*
+******************************************************************/
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/dashboard', {
     templateUrl: 'dashboard/dashboard.html',
@@ -9,6 +15,51 @@ angular.module('myApp.dashboard', ['ngRoute'])
   });
 }])
 
-.controller('DashboardCtrl', [function() {
+/*****************************************************************
+*
+* DashboardCtrl controlller
+*
+******************************************************************/
+.controller('DashboardCtrl', ['$scope', '$http', '$location', 'Config', function($scope, $http, $location, Config) {
+  
+  $scope.user = {
+    username: 'guest',
+    id: null,
+    email: null
+  }
+
+  $scope.errors = []
+
+  $scope.check = function () {
+
+    $http.defaults.cache = false
+
+    // Send a GET request to the authController
+    $http.get(Config.user_url)
+      // If success then...
+      .success(function(data, status, headers, config) {
+        console.log(data)
+        console.log(status)
+        if(status === 200 && data.user) {
+          $location.path('/dashboard')
+        }
+        else {
+          $scope.errors.push('Login appeared to succeed but something else has gone wrong')
+        }
+      })
+      // If error then...
+      .error(function(data, status, headers, config) {
+        console.log(data)
+        console.log(status)
+        if(status === 401) {
+          $scope.errors.push('Invalid credentials')
+        }
+        else {
+          $scope.errors.push('Login failed for unknown reason')
+        }
+      });
+  }
+
+  $scope.check()
 
 }]);
