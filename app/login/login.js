0c1d35604341b5078c786ec98f22178950d218a7
Laurence Elsdon <git@elsdon.me>
2015-03-18 00:54:12 +0000

    Login now functional

--- app/login/login.js
+++ app/login/login.js
@@ -9,6 +9,31 @@ angular.module('myApp.login', ['ngRoute'])
   });
 }])
 
-.controller('LoginCtrl', [function() {
+.controller('LoginCtrl', ['$scope', '$http', '$location', 'Config', function($scope, $http, $location, Config) {
+
+  $scope.errors = []
+
+  $scope.login = function () {
+    // Send a POST request to the authController
+    $http.post(Config.auth_url, $scope.user)
+      // If success then...
+      .success(function(data, status, headers, config) {
+        if(status === 200 && data.user) {
+          $location.path('/dashboard')
+        }
+        else {
+          $scope.errors.push('Login appeared to succeed but something else has gone wrong')
+        }
+      })
+      // If error then...
+      .error(function(data, status, headers, config) {
+        if(status === 401) {
+          $scope.errors.push('Invalid credentials')
+        }
+        else {
+          $scope.errors.push('Login failed for unknown reason')
+        }
+      });
+  }
 
 }]);
