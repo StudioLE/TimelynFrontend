6a34d2ee290f444ca9a5f646b90075aae7865997
Xysti Uno <xysti.uno@elsdon.me>
2015-04-01 00:48:37 +0100

    Minor updates

--- less.sh
+++ less.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Converting LESS to CSS"
-lessc -x /srv/sailstimeline/frontend/app/css/style.less /srv/sailstimeline/frontend/app/css/style.css
+lessc -x /srv/timelyn/frontend/app/css/style.less /srv/timelyn/frontend/app/css/style.css
