009b64800524d93172b2bd23749e9b466168a17d
Xysti Uno <xysti.uno@elsdon.me>
2015-03-31 03:56:44 +0100

    Fix typo

--- app/factories/eventFactory.js
+++ app/factories/eventFactory.js
@@ -8,7 +8,7 @@ angular.module('timelyn.eventFactory', [])
 *
 ******************************************************************/
 .factory('Event', function($resource, Config) {
-  return $resource(Config.url('/event/:id'), {}, {
+  return $resource(Config.app('/event/:id'), {}, {
     edit: { method: 'PUT', params: {id: '@id'} }
   });
 })
--- app/factories/timelineFactory.js
+++ app/factories/timelineFactory.js
@@ -8,7 +8,7 @@ angular.module('timelyn.timelineFactory', [])
 *
 ******************************************************************/
 .factory('Timeline', function($resource, Config) {
-  return $resource(Config.url('/timeline/:id'), {}, {
+  return $resource(Config.app('/timeline/:id'), {}, {
     edit: { method: 'PUT', params: {id: '@id'} }
   });
 })
