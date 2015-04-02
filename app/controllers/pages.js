40335aaf264788fed8198a0c8c24cb24331cf11f
Xysti Uno <xysti.uno@elsdon.me>
2015-04-02 02:36:12 +0100

    Revised pages

---
+++ app/controllers/pages.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('timelyn.pages', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/', {
+    templateUrl: 'views/pages/home.html',
+    controller: 'PageCtrl'
+  });
+  $routeProvider.when('/about', {
+    templateUrl: 'views/pages/about.html',
+    controller: 'PageCtrl'
+  });
+  $routeProvider.when('/pricing', {
+    templateUrl: 'views/pages/pricing.html',
+    controller: 'PageCtrl'
+  });
+  $routeProvider.when('/404', {
+    templateUrl: 'views/pages/404.html',
+    controller: '404Ctrl'
+  });
+}])
+
+.controller('PageCtrl', function(Breadcrumb) {
+  Breadcrumb.default()
+});
