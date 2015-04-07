774c9852d67a4d35bcc433d02e102602d274fe3e
Xysti Uno <xysti.uno@elsdon.me>
2015-04-07 23:08:57 +0100

    Bug fix

--- app/controllers/timeline/index.js
+++ app/controllers/timeline/index.js
@@ -25,9 +25,7 @@ angular.module('timelyn.timeline.index', ['ngRoute', 'ngResource', 'ui.bootstrap
   }
 
   // Get all Timelines for list
-  $scope.timelines = Timeline.query(function(timelines) {
-    console.log(timelines)
-  })
+  $scope.timelines = Timeline.query()
 
   Breadcrumb.default()
 
